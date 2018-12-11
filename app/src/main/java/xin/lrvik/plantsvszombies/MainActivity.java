2700837ed61bff4cd961fc696c7282799f2d6dff
豢涵 <huanhanfu@126.com>
2018-12-11 18:11:51 +0800

    first commit

---
+++ app/src/main/java/xin/lrvik/plantsvszombies/MainActivity.java
@@ -0,0 +1,12 @@
+package xin.lrvik.plantsvszombies;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
